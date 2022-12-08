5bc8434127cb3aa56db601554d65fbd48062fa36
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2022-12-08 11:22:55 +1100

    add vue-router and unit test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <RouterView v-slot="{ Component }" >
+      <component :is="Component" :key="$route.path"></component>
+    </RouterView>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RouterView
   }
 }
 </script>
